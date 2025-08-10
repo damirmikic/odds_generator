6b484175aa79ea80d25550d8cb6ceb2da2bb302e
damirmikic <damir@pixupplay.com>
2025-08-10 08:08:27 +0200

    Update fetch-events.js

--- netlify/functions/fetch-events.js
+++ netlify/functions/fetch-events.js
@@ -12,11 +12,9 @@ exports.handler = async (event, context) => {
   // Set up the API call parameters
   const from = Math.floor(Date.now() / 1000);
   const to = from + (48 * 3600); // 48 hours from now
-  const API_URL = `https://sports-api.cloudbet.com/pub/v2/odds/events?sport=soccer&from=${from}&to=${to}&live=false&markets=soccer.anytime_goalscorer&markets=soccer.match_odds&players=true&limit=1000`;
-  
-    console.log('Fetching API URL:', API_URL); // Log the URL for debugging
-  
-    try {
+  const API_URL = `https://sports-api.cloudbet.com/pub/v2/odds/events?sport=soccer&from=${from}&to=${to}&live=false&markets=soccer.anytime_goalscorer&markets=soccer.match_odds&players=true&limit=100`;
+
+  try {
       const response = await fetch(API_URL, {
         headers: { 'X-API-Key': API_KEY }
       });
